f09f0e81083c861a52e887e949e6edcafe8f7dc6
Jason <limaeight@gmail.com>
2025-01-25 23:25:13 -0500

    Done

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,12 +5,24 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
-    formatted_response = json.loads(response.text)
-    dominantemotion = ''
-    maxValue = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    for emotion in formatted_response['emotionPredictions'][0]['emotion']:
-        if formatted_response['emotionPredictions'][0]['emotion'][emotion] >= maxValue:
-            maxValue = formatted_response['emotionPredictions'][0]['emotion'][emotion]
-            dominantemotion = emotion
-    formatted_response['emotionPredictions'][0]['emotion']["dominant_emotion"] = dominantemotion
-    return formatted_response['emotionPredictions'][0]['emotion']
+
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        dominantemotion = ''
+        maxValue = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        for emotion in formatted_response['emotionPredictions'][0]['emotion']:
+            if formatted_response['emotionPredictions'][0]['emotion'][emotion] >= maxValue:
+                maxValue = formatted_response['emotionPredictions'][0]['emotion'][emotion]
+                dominantemotion = emotion
+        formatted_response['emotionPredictions'][0]['emotion']["dominant_emotion"] = dominantemotion
+        return formatted_response['emotionPredictions'][0]['emotion']
+    elif response.status_code == 400:
+        return '''{
+            "anger": "None",
+            "disgust": "None",
+            "fear": "None",
+            "joy": "None",
+            "sadness": "None",
+            "dominant_emotion": "None"
+        }'''
+        
--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
+import json
 
 app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def emot_analyzer():
     text_to_analyze = request.args.get('textToAnalyze')
-
     response = emotion_detector(text_to_analyze)
+
+    if json.loads(response)["dominant_emotion"] == 'None':
+        return "Invalid text! Please try again"
 
     return f"For the given statement, the system response is \
     'anger': {response['anger']}, \
